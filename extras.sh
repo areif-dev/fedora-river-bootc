0a0a8ccfaa8302737e413aadb6f80b2dcdee3951
areif-dev <aj@ajreifsnyder.com>
2025-01-20 13:13:57 -0500

    Extras: Install nwg-look

--- extras.sh
+++ extras.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash 
 
 # Install Starship prompt
+echo "Attempting to install Starship Prompt"
 STARSHIP_VERSION=$(curl -s "https://api.github.com/repos/starship/starship/releases/latest" | jq -r '.tag_name' | sed 's/v//')
 curl -L -o "/tmp/extras/starship-x86_64-unknown-linux-gnu.tar.gz" "https://github.com/starship/starship/releases/download/v${STARSHIP_VERSION}/starship-x86_64-unknown-linux-gnu.tar.gz"
 curl -L -o "/tmp/extras/starship-x86_64-unknown-linux-gnu.tar.gz.sha256" "https://github.com/starship/starship/releases/download/v${STARSHIP_VERSION}/starship-x86_64-unknown-linux-gnu.tar.gz.sha256"
@@ -14,3 +15,18 @@ else
     echo "Starship checksums do not match"
     exit 1
 fi
+
+# Install nwg-look 
+echo "Attempting to install NWG-Look"
+mkdir -p /tmp/extras/nwg-look
+# NWG_LOOK_VERSION=$(curl -s "https://api.github.com/repos/nwg-piotr/nwg-look/releases/latest" | jq -r '.tag_name' | sed 's/v//')
+NWG_LOOK_VERSION="0.2.7"
+curl -Lo "/tmp/extras/nwg-look.tar.gz" "https://github.com/nwg-piotr/nwg-look/releases/download/v${NWG_LOOK_VERSION}/nwg-look-v${NWG_LOOK_VERSION}_x86_64.tar.gz"
+tar -xzf "/tmp/extras/nwg-look.tar.gz" -C "/tmp/extras/nwg-look"
+mkdir -p /usr/share/nwg-look/langs
+mkdir -p /usr/share/applications
+cp /tmp/extras/nwg-look/stuff/main.glade /usr/share/nwg-look/
+cp /tmp/extras/nwg-look/langs/* /usr/share/nwg-look/langs/
+cp /tmp/extras/nwg-look/stuff/nwg-look.desktop /usr/share/applications/
+cp /tmp/extras/nwg-look/stuff/nwg-look.svg /usr/share/pixmaps/
+cp /tmp/extras/nwg-look/nwg-look /usr/bin
